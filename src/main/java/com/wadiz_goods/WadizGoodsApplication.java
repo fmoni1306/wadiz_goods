f96762d8d2709b4535e6a2ef823a0bb6cbb9b094
yoon <fmoni1306@gmail.com>
2021-07-05 22:22:05 +0900

    Initial commit

---
+++ src/main/java/com/wadiz_goods/WadizGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.wadiz_goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadizGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WadizGoodsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wadiz_goods/WadizGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wadiz_goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WadizGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
