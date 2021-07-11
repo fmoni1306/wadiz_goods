d824a23981911b4a40ff4997019619e8ddabe0e3
Yoon <fmoni1306@gmail.com>
2021-07-12 00:59:19 +0900

    commit test

--- src/main/java/com/wadiz_goods/domain/Project.java
+++ src/main/java/com/wadiz_goods/domain/Project.java
@@ -1,4 +1,14 @@
 package com.wadiz_goods.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Project {
+
+    @Id @GeneratedValue
+    @Column(name = "project_id")
+    private Long id;
 }
